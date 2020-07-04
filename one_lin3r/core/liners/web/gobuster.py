ed702b44f988e4090a4e3fc7c891b061b37db21b
Andy Feng <ayfeng@uci.edu>
2020-07-03 18:46:48 -0700

    Add web/gobuster

---
+++ one_lin3r/core/liners/web/gobuster.py
@@ -0,0 +1,5 @@
+class info:
+    author      = "snix0"
+    description = "Fuzz hidden directories with gobuster"
+    function    = "Web"
+    liner       = "gobuster dir -u 'http://TARGET/' -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -o gobuster.80"
